ec3f768cfc0948bd459b2893626d329f4d3c3dba
ROBINSON MOLINA <robinson.granados01@gmail.com>
2026-03-31 01:32:54 -0500

    se agrego el modelo para los mensajes

---
+++ sistermaB/src/modelos/mensaje.modelo.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose';
+
+const mensajeSchema = new mongoose.Schema({
+    contenido: {
+        type: String,
+        required: true
+    },
+    emisor: {
+        type: String,
+        required: true
+    },
+    enviadoEl: {
+        type: String, // Timestamp exacto en el que el Sistema A realizó el envío
+        required: true
+    },
+    hashRecibido: {
+        type: String,
+        required: true
+    },
+    estadoIntegridad: {
+        type: String,
+        enum: ['Íntegro', 'Alterado'],
+        default: 'Íntegro'
+    }
+}, {
+    timestamps: true // Fecha en la que el Sistema B recibió y guardó el registro
+});
+
+export default mongoose.model('Mensaje', mensajeSchema);
